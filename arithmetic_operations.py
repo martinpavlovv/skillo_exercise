b24d82e78f3240a6cfb6bf71bef57b42cccae8a9
martipi4a <martinpavlovv@gmail.com>
2023-10-11 17:10:57 +0300

    homework1

---
+++ arithmetic_operations.py
@@ -0,0 +1,7 @@
+number_1 = int(input())
+number_2 = int(input())
+
+print('The sum of', number_1, 'and', number_2, 'is', number_1+number_2 )
+print('The difference between', number_1, 'and', number_2, 'is', number_1-number_2 )
+print('The result of multiplication between', number_1, 'and', number_2, 'is', number_1*number_2 )
+print('The result of division between', number_1, 'and', number_2, 'is', number_1/number_2 )
